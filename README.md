f9770794d522a8b569ad0aac5fc594cb8f8db647
Frank Bryce <jonnyjack7@gmail.com>
2022-04-28 16:20:04 -0400

    Build relatively simple assembly index algorithm.
    It's not fast, but I guess np hard problems can't be fast. It's fast
    enough for abracadabra anyways.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # assembly-theory
 Working on algorithm to find complexity of strings by https://en.wikipedia.org/wiki/Assembly_theory
+
+Using this algorithm, I discovered there are many assembly paths for abracadabra of index 7.
+
+```bash
+$ python3 assembly.py 
+print all min assembly paths for 'abracadabra'
+index of 7: (((a|b)|(r|a))|((c|a)|(d|((a|b)|(r|a)))))
+index of 7: ((a|(b|(r|a)))|(c|((a|d)|(a|(b|(r|a))))))
+index of 7: (((a|b)|(r|a))|(c|(a|(d|((a|b)|(r|a))))))
+index of 7: (((a|b)|(r|a))|(c|((a|d)|((a|b)|(r|a)))))
+index of 7: ((a|(b|(r|a)))|(c|(a|(d|(a|(b|(r|a)))))))
+index of 7: (((a|b)|(r|a))|((c|(a|d))|((a|b)|(r|a))))
+index of 7: ((a|(b|(r|a)))|((c|(a|d))|(a|(b|(r|a)))))
+index of 7: ((a|(b|(r|a)))|((c|a)|(d|(a|(b|(r|a))))))
+```
--- assembly.py
+++ assembly.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 class Path:
-    idx: int
     obj: str
     bks: set[Path]  # for caching only
     left: Path
@@ -11,39 +10,80 @@ class Path:
         self.obj = obj
         self.left = left
         self.right = right
-        max_idx = 0
-        if left:
-            max_idx = max(max_idx, 1 + left.idx)
-        if right:
-            max_idx = max(max_idx, 1 + right.idx)
-        self.idx = max_idx
+        if (not left and right) or (left and not right):
+            raise ValueError('must specify both left and right, or neither.')
         self.bks = set([self])
         if left:
             self.bks |= left.bks
-        if right:
             self.bks |= right.bks
 
-    def __eq__(self, other: Path):
+    def __eq__(self, other: Path) -> bool:
         return self.__hash__() == other.__hash__()
 
-    def __hash__(self):
-        lhash = 0 if not self.left else self.left.__hash__()
-        rhash = 0 if not self.right else self.right.__hash__()
-        return hash((self.idx,self.obj,lhash,rhash))
+    def __hash__(self) -> int:
+        return hash(self.obj)
 
-    def __str__(self):
-        return f'index of {self.idx}: {self.obj}'
+    def __str__(self) -> str:
+        return f'index of {self.Index()}: {self.obj}'
+
+    def Index(self, exists: set[Path] = set()) -> int:
+        l = self.left
+        r = self.right
+        if (not l and r) or (l and not r):
+            raise ValueError('Invalid State: had only one of self.left and self.right set')
+        if not l and not r:
+            return 0  # this is an atom
+
+        # assemble right from left's building blocks
+        lr_idx = 1  # take into account this assembly step.
+        if l not in exists:
+            lr_idx += l.Index()
+        lr_idx += r.Index(exists | l.bks)
+
+        # assemble left from right's building blocks
+        rl_idx = 1
+        if r not in exists:
+            rl_idx += r.Index()
+        rl_idx += l.Index(exists | r.bks)
+
+        return min(lr_idx, rl_idx)
+
+    def raw(self, concat: str = '|', lbr: str = '(', rbr: str = ')') -> str:
+        return self.obj.translate({ord(lbr): None, ord(rbr): None, ord(concat): None})
 
     @staticmethod
     def Atom(s: str) -> Path:
         return Path(s, None, None)
 
     @staticmethod
-    def From(left: Path, right: Path):
-        return Path(f'({left.obj}|{right.obj})', left, right)
+    def From(left: Path, right: Path, concat: str = '|', lbr: str = '(', rbr: str = ')') -> Path:
+        return Path(f'{lbr}{left.obj}{concat}{right.obj}{rbr}', left, right)
 
-if __name__ == "__main__":
-    # make the object that represents abracadabra
+def MinStrPaths(s: str) -> list[Path]:
+    layers: list[set[Path]] = list()
+    atoms = set([Path.Atom(c) for c in s])
+    layers.append(atoms)
+    for l in range(1,len(s)):
+        next_layer = set()
+        for i in range(int((l+1)/2)):
+            l1 = layers[i]
+            l2 = layers[l-i-1]
+            next_layer |= set([Path.From(o1,o2) for o1 in l1 for o2 in l2 if o1.raw()+o2.raw() in s])
+        layers.append(next_layer)
+    
+    minPaths = list()
+    minIdx = len(s)+1
+    for path in layers[len(s)-1]:
+        pidx = path.Index()
+        if pidx < minIdx:
+            minIdx = pidx
+            minPaths = [path]
+        elif pidx == minIdx:
+            minPaths.append(path)
+    return minPaths
+
+def genearateAbracadabra() -> Path:
+    # generate and return the Path that represents 'abracadabra'
     a = Path.Atom('a') # idx = 0
     b = Path.Atom('b') # idx = 0
     r = Path.Atom('r') # idx = 0
@@ -57,3 +97,8 @@ if __name__ == "__main__":
     abracad = Path.From(abraca,d) # idx = 6
     abracadabra = Path.From(abracad,abra) # idx = 7
     print(abracadabra)
+
+if __name__ == "__main__":
+    print("print all min assembly paths for 'abracadabra'")
+    for path in MinStrPaths('abracadabra'):
+        print(path)
