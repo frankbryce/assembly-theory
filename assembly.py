ada7324913b64f5a07216938d87826fcf07b49ce
Frank Bryce <jonnyjack7@gmail.com>
2022-04-29 14:56:34 -0400

    Modifying innards of the Assembly object to use a graph instead of str.
    This relies on the networkx library to do. As a first commit, I assume a
    linear, ordered, object like a string but this will be extended to
    support other graphs, too. This is quite slow compared to before, but
    more versitile. I'm hoping to leverage networkx's vast library of
    algorithms to help the optimization process.
    
    First idea: instead of bottom-up path construction, do a top-down path
    construction instead. I think this will be ~as fast but there may be
    cleverer path pruning optimizations. There's a 200x speed decrease in
    this change that I gotta fix.

--- assembly.py
+++ assembly.py
@@ -2,42 +2,60 @@ from __future__ import annotations
 
 from functools import lru_cache
 cache = lru_cache(maxsize=None)
+import networkx as nx
 import sys
 from tqdm import tqdm
 
 class Assembly:
-    val: str
+    atoms: nx.MultiGraph
 
-    def __init__(self, val) -> Assembly:
-        self.val = val
+    @staticmethod
+    def Atom(element: str):
+        ret = Assembly()
+        ret.atoms = nx.MultiGraph()
+        ret.atoms.add_nodes_from([(0, {'element': element})])
+        return ret
+
+    @staticmethod
+    def Concat(left: Assembly, right: Assembly) -> Assembly:
+        ret = Assembly()
+        lsz = len(left.atoms)
+        rsz = len(right.atoms)
+        rcpy = right.atoms.copy()
+        nx.relabel_nodes(rcpy, dict([(i,i+lsz) for i in range(rsz)]), copy=False)
+        ret.atoms = nx.compose(left.atoms,rcpy)
+        ret.atoms.add_edge(lsz-1,lsz)
+        return ret
+
+    def is_atom(self) -> bool:
+        return len(self.atoms) == 1
 
     def __lt__(self, other: Assembly) -> bool:
-        return self.val < other.val
+        return self.__hash__() < other.__hash__()
 
     def __eq__(self, other: Assembly) -> bool:
-        return self.val == other.val
+        return self.__hash__() == other.__hash__()
         
     def __hash__(self):
-        return hash(self.val)
+        return int(nx.weisfeiler_lehman_graph_hash(self.atoms, node_attr='element'),16)
 
     def __str__(self) -> str:
-        return self.val
+        return ''.join([a['element'] for _,a in sorted(self.atoms.nodes(data=True), key=lambda n: n[0])])
 
 class Path:
     idx: int
-    obj: Assembly
+    asm: Assembly
     bks: set[Assembly]  # cache of blocks used to build this path
     left: Path
     right: Path
 
-    # construct Path from adding an object "right"
-    def __init__(self, obj: Assembly, left: Path, right: Path) -> Path:
-        self.obj = obj
+    def __init__(self, asm: Assembly, left: Path, right: Path) -> Path:
+        self.asm = asm
         self.left = left
         self.right = right
         if (not left and right) or (left and not right):
             raise ValueError('must specify both left and right, or neither.')
-        self.bks = set([obj])
+        self.bks = set([asm])
         if left:
             self.bks |= left.bks
             self.bks |= right.bks
@@ -46,16 +64,16 @@ class Path:
         return self.__hash__() == other.__hash__()
 
     def __hash__(self) -> int:
-        return hash((self.obj,tuple(sorted(self.bks))))
+        return hash((self.asm,tuple(sorted(self.bks))))
 
     def __str__(self) -> str:
         if not self.left:
-            return f'{self.Index()}:{self.obj}'
+            return f'{self.Index()}:{self.asm}'
         return f'{self.Index()}:({self.left}|{self.right})'
 
     @cache
     def Index(self, exists: tuple[Assembly] = tuple()) -> int:
-        if self.obj in exists:
+        if self.asm in exists:
             return 0
 
         l = self.left
@@ -78,26 +96,31 @@ class Path:
         return min(lr_idx, rl_idx)
 
     @staticmethod
-    def Atom(s: str) -> Path:
-        return Path(Assembly(s), None, None)
+    def From(a: Assembly) -> Path:
+        if not a.is_atom():
+            raise ValueError("From() must be called with an Atom Assembly")
+        return Path(a, None, None)
 
+    # This algorithm assumes linear, ordered assemblies (aka strings)
     @staticmethod
-    def Join(left: Path, right: Path, concat: str = '', lbr: str = '', rbr: str = '') -> Path:
-        val = f'{lbr}{left.obj}{concat}{right.obj}{rbr}'
-        return Path(Assembly(val), left, right)
+    def Concat(left: Path,
+               right: Path) -> Path:
+        return Path(Assembly.Concat(left.asm,right.asm), left, right)
 
 def MinStrPaths(s: str) -> list[Path]:
     layers: list[set[Path]] = list()
-    atoms = set([Path.Atom(c) for c in s])
+    atoms = set([Path.From(Assembly.Atom(c)) for c in s])
     layers.append(atoms)
     for l in tqdm(range(1,len(s)), "Generating Paths: "):
         next_layer = set()
         for i in range(int((l+1)/2)):
             l1 = layers[i]
             l2 = layers[l-i-1]
-            next_layer |= set([Path.Join(o1,o2) for o1 in l1 for o2 in l2 if str(o1.obj)+str(o2.obj) in s])
+            next_layer |= set([
+                Path.Concat(o1,o2)
+                for o1 in l1 for o2 in l2 if str(o1.asm)+str(o2.asm) in s])
         layers.append(next_layer)
-    
+
     minPaths = list()
     minIdx = len(s)+1
     for path in tqdm(layers[len(s)-1], "Looking for min index"):
@@ -109,20 +132,20 @@ def MinStrPaths(s: str) -> list[Path]:
             minPaths.append(path)
     return minPaths
 
-def genearateAbracadabra() -> Path:
+def generateAbracadabra() -> Path:
     # generate and return the Path that represents 'abracadabra'
-    a = Path.Atom('a') # idx = 0
-    b = Path.Atom('b') # idx = 0
-    r = Path.Atom('r') # idx = 0
-    c = Path.Atom('c') # idx = 0
-    d = Path.Atom('d') # idx = 0
-    ab = Path.Join(a,b) # idx = 1
-    abr = Path.Join(ab,r) # idx = 2
-    abra = Path.Join(abr,a) # idx = 3
-    abrac = Path.Join(abra,c) # idx = 4
-    abraca = Path.Join(abrac,a) # idx = 5
-    abracad = Path.Join(abraca,d) # idx = 6
-    abracadabra = Path.Join(abracad,abra) # idx = 7
+    a = Path.From(Assembly.Atom('a')) # idx = 0
+    b = Path.From(Assembly.Atom('b')) # idx = 0
+    r = Path.From(Assembly.Atom('r')) # idx = 0
+    c = Path.From(Assembly.Atom('c')) # idx = 0
+    d = Path.From(Assembly.Atom('d')) # idx = 0
+    ab = Path.Concat(a,b) # idx = 1
+    abr = Path.Concat(ab,r) # idx = 2
+    abra = Path.Concat(abr,a) # idx = 3
+    abrac = Path.Concat(abra,c) # idx = 4
+    abraca = Path.Concat(abrac,a) # idx = 5
+    abracad = Path.Concat(abraca,d) # idx = 6
+    abracadabra = Path.Concat(abracad,abra) # idx = 7
     print(abracadabra)
 
 if __name__ == "__main__":
